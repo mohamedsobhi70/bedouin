07ce76581bb79ab79e02ea51e7e24883185e9d20
mohamedsobhi70 <sobhi7070@gmail.com>
2023-05-14 01:28:37 +0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         'Recoleta': ['Recoleta', 'sans-serif'],
       },
+      backgroundImage: {
+        'room-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) -6.78%, #000000 188.98%)',
+      }
     },
   },
   plugins: [],
